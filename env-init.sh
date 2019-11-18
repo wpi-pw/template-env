fa36c936220f23d9974809e010b0d0a6a9ea9c08
DimaMinka <cdk@cdk.co.il>
2019-11-18 21:38:43 +0200

    feat--env-init--extra conf support

--- env-init.sh
+++ env-init.sh
@@ -57,3 +57,42 @@ echo "LOGGED_IN_SALT=\"$(cat $app_path/salts.txt |grep -w LOGGED_IN_SALT | cut -
 echo "NONCE_SALT=\"$(cat $app_path/salts.txt |grep -w NONCE_SALT | cut -d \' -f 4)\"" >>  $app_path/.env
 # remove key file
 rm $app_path/salts.txt
+
+echo "" >> $app_path/.env
+
+# extra custom config
+echo "EXTRA_FILES=$(wpi_yq extra.files)" >> $app_path/.env
+echo "WPI_HOST=$(wpi_key "app_host")" >> $app_path/.env
+echo "WP_POST_REVISIONS=$(wpi_yq extra.wp_post_revisions)" >> $app_path/.env
+echo "AUTOSAVE_INTERVAL=$(wpi_yq extra.autosave_interval)" >> $app_path/.env
+echo "WP_MEMORY_LIMIT=$(wpi_yq extra.wp_memory_limit)" >> $app_path/.env
+echo "WP_MAX_MEMORY_LIMIT=$(wpi_yq extra.wp_max_memory_limit)" >> $app_path/.env
+echo "DISABLE_WP_CRON=$(wpi_yq extra.wp_cron_disable)" >> $app_path/.env
+
+echo "" >> $app_path/.env
+
+# extra custom config for production
+echo "MEDIA_TRASH=$(wpi_yq extra.media_trash)" >> $app_path/.env
+echo "EMPTY_TRASH_DAYS=$(wpi_yq extra.empty_trash_days)" >> $app_path/.env
+
+echo "" >> $app_path/.env
+
+# extra multisite config
+echo "EXTRA_MULTISITE=$(wpi_yq extra.multisite)" >> $app_path/.env
+
+echo "" >> $app_path/.env
+
+# WP Mail SMTP by WPForms config
+echo "WPMS_ON=$(wpi_yq extra.wpms_on)" >> $app_path/.env
+echo "WPMS_SMTP_PASS=$(wpi_yq extra.wpms_smtp_pass)" >> $app_path/.env
+
+echo "" >> $app_path/.env
+
+# WP Rocket license config
+echo "WP_ROCKET_EMAIL=$(wpi_yq extra.wp_rocket_email)" >> $app_path/.env
+echo "WP_ROCKET_KEY=$(wpi_yq extra.wp_rocket_key)" >> $app_path/.env
+
+echo "" >> $app_path/.env
+
+# AdvancedCustomFields Pro - license config
+echo "ACF_PRO_UPDATE_LICENSE=$(wpi_yq extra.acf_pro_update_license)" >> $app_path/.env
